d04fe1ffc69318aa1a94aaac615b7e0bedb957b9
Andy Reding <areding99@gmail.com>
2023-04-11 15:44:55 +0100

    initial commit

---
+++ src/Library/Library.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./Library.css";
+import Logo from "../Logo/Logo";
+
+const App: React.FC = () => {
+  return <Logo />;
+};
+
+export default App;
---
+++ src/Logo/Logo.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Logo: React.FC = () => {
+  return <p>check this out</p>;
+};
+
+export default Logo;
